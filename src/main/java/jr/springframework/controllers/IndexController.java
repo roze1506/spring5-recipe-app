9dd51f7c435d2c112ba4dde0f6ca96988fa6cdf9
JeffreyRozendaal <jeffreyrozendaal10@msn.com>
2020-09-08 21:07:03 +0200

    Section 7 Created Index page

---
+++ src/main/java/jr/springframework/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package jr.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String geefIndexPaginaTerug() {
+        return "index";
+    }
+}
