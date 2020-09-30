92b27f311d5beeabe84a1208f30baf286abff0b7
JeffreyRozendaal <jeffreyrozendaal10@msn.com>
2020-09-30 20:13:06 +0200

    Section 10 ArgumentCapturer added to IndexControllerTest unit test.

--- src/test/java/jr/springframework/controllers/IndexControllerTest.java
+++ src/test/java/jr/springframework/controllers/IndexControllerTest.java
@@ -6,6 +6,7 @@ import jr.springframework.services.ReceptService;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -29,12 +30,18 @@ public class IndexControllerTest {
         // Prepare
         final Model model = Mockito.mock(Model.class);
         final List<Recept> testRecepten = new ArrayList<>();
+        testRecepten.add(new Recept());
+        final Recept testRecept = new Recept();
+        testRecept.setId(5L);
+        testRecepten.add(testRecept);
+        final ArgumentCaptor<List<Recept>> argumentCaptor = ArgumentCaptor.forClass(List.class);
         Mockito.when(receptService.krijgAlleRecepten()).thenReturn(testRecepten);
         // Execute
         final String resultaat = indexController.geefIndexPaginaTerug(model);
         // Verify
         Mockito.verify(receptService).krijgAlleRecepten();
-        Mockito.verify(model).addAttribute("recepten", testRecepten);
+        Mockito.verify(model).addAttribute(Mockito.eq("recepten"), argumentCaptor.capture());
         Assertions.assertThat(resultaat).isEqualTo("index");
+        Assertions.assertThat(argumentCaptor.getValue()).hasSize(2);
     }
 }
